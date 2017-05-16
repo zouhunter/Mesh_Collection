4774ab73b18b22a2c52d4d78d33c7927300b9fa7
zouhangte <zouhangtezbm@126.com>
2017-05-16 13:59:00 +0800

    remove err

--- Assets/BigDemo/FiniteElement/Scripts/_Entry/Develop.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Events;
-using System.Collections.Generic;
-
-public class Develop :SceneMain<Develop> {
-    public BeamSystem beamSystem;
-    public SteelSystem steelSystem;
-}
