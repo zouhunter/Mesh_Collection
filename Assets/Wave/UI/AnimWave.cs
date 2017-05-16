6599b4d5c6651a57cfa21d9270836d23de7f8ac9
zouhunter <106362705@qq.com>
2017-05-17 00:10:56 +0800

    实现左右移动

--- Assets/Wave/UI/AnimWave.cs
+++ Assets/Wave/UI/AnimWave.cs
@@ -4,61 +4,136 @@ using UnityEngine.Events;
 using System.Collections.Generic;
 public class AnimWave : MaskableGraphic
 {
-    [Range(0.01f, 100)]
-    public float speed = 1f;
-    public int count = 30;
-    public float emptyHeight = 10;
-    [Range(0.01f,1f)]
-    public float rangeHeight = 0.01f;
+    [Range(0.01f, 1)]
+    public float updatetime = 1f;
+    private float updatetimer;
+    public int scaler = 30;
+    public float height = 5;
+    public float defultHight = 10;
+    public bool toleft;
+    [Range(0.01f, 10f)]
+    public float rangewave = 0.01f;
     public AnimationCurve waveCurve;
     private float scale = 0f;
     private Vector2 size;
-    private Keyframe[] keyframes;
-    private int timer = 1;
-    private float waveRange;
+    private float rangewaveTimer;
+    private bool rising;
+    private float maxAnim;
     VertexHelper VH { get; set; }
+    private List<Vector2[]> posList = new List<Vector2[]>();
+    private float _index;
     protected override void Awake()
     {
         base.Awake();
-        keyframes = waveCurve.keys;
+        VH = null;
+        maxAnim = waveCurve.keys[waveCurve.length - 1].time;
     }
     protected override void OnPopulateMesh(VertexHelper vh)
     {
         size = GetComponent<RectTransform>().rect.size;
+        scale = size.x / scaler;
         vh.Clear();
-        scale = size.x / count;
-        float randomheight = 0;
-        for (int i = 0; i < count; i++)
-        {
-            var x1 = i * scale;
-            var x2 = (i + 1) * scale;
-            if (i % 5 == 0)
-            {
-                randomheight = UnityEngine.Random.Range(0, rangeHeight);
-            }
-            Vector2 pos1 = new Vector2(x1, 0);
-            Vector2 pos2 = pos1 + Vector2.up * (GetCurveHeight(x1, Time.time * speed, i));
-            Vector2 pos4 = new Vector2(x2, 0);
-            Vector2 pos3 = pos4 + Vector2.up * (GetCurveHeight(x2, Time.time * speed, i));
 
-            vh.AddUIVertexQuad(GetQuad(pos1, pos2, pos3, pos4));
+        if (VH == null)
+        {
+            if (toleft)
+            {
+                for (int i = 0; i <= scaler; i++)
+                {
+                    _index += scale;
+
+                    Vector2 pos1 = new Vector2(_index, 0);
+                    Vector2 pos2 = pos1 + Vector2.up * (GetCurveHeight(_index));
+                    var node = new Vector2[] { pos1, pos2 };
+                    posList.Add(node);
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= scaler; i++)
+                {
+                    _index = i * scale;
+
+                    Vector2 pos1 = new Vector2(_index, 0);
+                    Vector2 pos2 = pos1 + Vector2.up * (GetCurveHeight((scaler - i) * scale));
+                    var node = new Vector2[] { pos1, pos2 };
+                    posList.Add(node);
+                }
+            }
+
+        }
+        else
+        {
+            if (toleft)
+            {
+                for (int i = 0; i <= scaler; i++)
+                {
+                    if (i != scaler)
+                    {
+                        posList[i][1].y = posList[i + 1][1].y;
+                    }
+                    else
+                    {
+                        _index += scale;
+                        posList[i][1].y = GetCurveHeight(_index);
+                    }
+                }
+            }
+
+            else
+            {
+                for (int i = scaler; i >= 0; i--)
+                {
+                    if (i != 0)
+                    {
+                        posList[i][1].y = posList[i - 1][1].y;
+                    }
+                    else
+                    {
+                        _index += scale;
+                        posList[0][1].y = GetCurveHeight(_index);
+                    }
+                }
+            }
+
         }
 
+        for (int i = 0; i < scaler; i++)
+        {
+            vh.AddUIVertexQuad(GetQuad(posList[i][0], posList[i][1], posList[i + 1][1], posList[i + 1][0]));
+        }
         VH = vh;
     }
     private void Update()
     {
-        if (VH != null){
+        updatetimer += Time.deltaTime;
+
+        if (VH != null && updatetimer > updatetime)
+        {
+            updatetimer = 0;
             this.SetAllDirty();
         }
-        if (Time.time % timer < 0.02f)
-        {
-            waveRange = Random.Range(0,rangeHeight);
-        }
+
     }
-    private float GetCurveHeight(float normalized_x, float time,int index)
+    private float GetCurveHeight(float normalized_x)
     {
-        return waveCurve.Evaluate(normalized_x + time) + emptyHeight + waveRange * Mathf.Abs(count * 0.5f - index);
+        if (rangewaveTimer > rangewave)
+        {
+            rising = false;
+        }
+        if (rangewaveTimer < -rangewave)
+        {
+            rising = true;
+        }
+        if (rising)
+        {
+            rangewaveTimer += Time.deltaTime;
+        }
+        else
+        {
+            rangewaveTimer -= Time.deltaTime;
+        }
+        return waveCurve.Evaluate(normalized_x) * height + defultHight + rangewaveTimer;
     }
 
     private UIVertex[] GetQuad(params Vector2[] vertPos)
