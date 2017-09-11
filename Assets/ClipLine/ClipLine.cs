82c453345845466be072393558c22f1d1f1e817e
zouhangte <zouhantezbm@126.com>
2017-09-11 14:24:18 +0800

    修正空间距离问题

--- Assets/ClipLine/ClipLine.cs
+++ Assets/ClipLine/ClipLine.cs
@@ -5,9 +5,16 @@ using System.Collections;
 public class ClipLine : MonoBehaviour
 {
     public string matName;
-    public float speed;
+    public float speed = 0.2f;
+    public bool once = true;
+    public float span = 0.01f;
+
     private Material mat;
-    private float timer;
+    private float lastdistence;
+    private float distenceAll;
+    private float distence;
+    private Vector4[] posList;
+    private float[] distenceList;
     void Start()
     {
         Renderer render = GetComponent<Renderer>();
@@ -20,27 +27,80 @@ public class ClipLine : MonoBehaviour
             mat = Array.Find<Material>(render.materials, x => x.name == matName);
         }
 
-        if (mat)
+        if (!mat || transform.childCount < 2)
         {
-            //mat = new Material(mat.shader);
-            mat.SetInt("_Count", transform.childCount);
+            this.enabled = false;
+        }
+        else
+        {
+            distenceList = new float[transform.childCount - 1];
             for (int i = 0; i < transform.childCount; i++)
             {
-                var pos = transform.GetChild(i).localPosition;
-                mat.SetVector("_Pos" + i, pos);
+                if (i > 0)
+                {
+                    distenceList[i - 1] = Vector3.Distance(transform.GetChild(i - 1).position, transform.GetChild(i).position);
+                    distenceAll += distenceList[i - 1];
+                }
             }
         }
-
-
     }
 
     void Update()
     {
-        timer += Time.deltaTime * speed;
-        if (timer > 1){
-            timer = 0;
+        distence += Time.deltaTime * speed;
+        if (distence > distenceAll)
+        {
+            distence = 0;
+            if (once)
+            {
+                mat.SetInt("_Count", 0);
+                enabled = false;
+                return;
+            }
         }
-        mat.SetFloat("_Slider", timer);
-}
 
+        if (Mathf.Abs(distence - lastdistence) > 0.01f)
+        {
+            CalcutPoistionList();
+            OnPositionChanged();
+            lastdistence = distence;
+        }
+
+    }
+
+    void CalcutPoistionList()
+    {
+        float distenceTemp = 0f;
+        int count = 1;
+        for (int i = 0; i < distenceList.Length; i++)
+        {
+            count++;
+
+            distenceTemp += distenceList[i];
+
+            if (distenceTemp > distence)
+            {
+                distenceTemp -= distenceList[i];
+                    break;
+            }
+        }
+        posList = new Vector4[count];
+        int index = 0;
+        for (; index < posList.Length - 1; index++)
+        {
+            posList[index] = transform.GetChild(index).localPosition;
+            posList[index].w = 1;
+        }
+        posList[index] = Vector4.Lerp(transform.GetChild(index - 1).localPosition, transform.GetChild(index).localPosition,
+            (distence - distenceTemp) / distenceList[index - 1]);
+    }
+    void OnPositionChanged()
+    {
+        mat.SetInt("_Count", posList.Length);
+
+        for (int i = 0; i < posList.Length; i++)
+        {
+            mat.SetVector("_Pos" + i, posList[i]);
+        }
+    }
 }
